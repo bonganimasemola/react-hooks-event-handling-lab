f8d95b43e636cbe72ad175044d30f13279d19995
bonganimasemola <bonganimasemola@icloud.com>
2023-10-27 12:14:20 +0300

    i think i got it

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Keypad from './Keypad';
+import EyesOnMe from './EyesOnMe';
+
+export default function App() {
+  return (
+    <div>App
+        <Keypad />
+        <EyesOnMe />
+    </div>
+  )
+}
+
+
+default export App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
-// Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+
+function Keypad() {
+    function handleChange() {
+        console.log('Entering password...');
+      }
+    
+  return (
+    <div>
+      <label htmlFor="password" >Password: </label>
+      <input
+        type="password"
+        id="password"
+        onChange={handleChange}
+        placeholder="enter password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
